04839e352147173fdf536760be1de0c73f9de9d5
Andrea Acosta Becerra <andrea.acosta@aguayopublicidad.com>
2018-05-08 15:08:53 -0500

    [Transversal] [Vue]  - se habilita modulo Avatar de usuario para el administrador

---
+++ src/components/UserAvatar.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  figure.avatar(
+    v-if='user.id')
+    //-vue variable Notificaciones usuario
+    img.avatar__img(
+      v-if='user.picture'
+      :src='user.picture',
+      :alt='user.first_name')
+    span.avatar__letter(
+      v-else) {{ user.first_name.charAt(0) }}
+    //-vue variable user name
+    figcaption.avatar__txt  {{ user.first_name }}
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'UserAvatar',
+  computed: {
+    ...mapState(['user'])
+  }
+}
+</script>
