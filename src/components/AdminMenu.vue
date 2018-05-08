4bf856b7ae987be48026a25ff70688e39bc05ce3
Andrea Acosta Becerra <andrea.acosta@aguayopublicidad.com>
2018-05-08 15:21:33 -0500

    [Menu] [Vue]  - Update link para ventas

--- src/components/AdminMenu.vue
+++ src/components/AdminMenu.vue
@@ -37,7 +37,7 @@ export default {
             },
             {
               name: 'Ventas',
-              url: '#',
+              url: '/sales',
               ico: 'i-sale'
             },
             {
