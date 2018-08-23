b01c37b85a2c57ec56838340df7dc3556482c680
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2018-08-23 10:54:00 -0500

    Code cleanup

--- src/components/EditSize.vue
+++ src/components/EditSize.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   .edit__slide
     h3.slide__header.i-close(
-      @click.stop="$emit('close')") Editar marca
+      @click.stop="$emit('close')") Editar talla
     form.slide__form(@submit.prevent="submit")
       .form__row
         label.form__label(
