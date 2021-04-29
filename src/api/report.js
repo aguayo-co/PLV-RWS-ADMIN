f96e8b3fbe9103a7b4085fb92b81f974b05b340b
Deybith <stivensimijaca54@gmail.com>
2021-04-29 15:44:01 -0500

    more parts in report request

--- src/api/report.js
+++ src/api/report.js
@@ -1,7 +1,21 @@
 /**
  * API Calls related to orders and their properties
  */
-import Vue from 'vue'
+import Vue from 'vue';
+
+const getInParts = (part = null, params, nexResponse = {}) => {
+  if (part) {
+    return Vue.axiosAuth.post(`/api/report/${part}`, nexResponse, { params })
+      .then(({ data }) => {
+        if (data.nextPart) {
+          return getInParts(data.nextPart, params, { nexResponse: data.nexResponse })
+        }
+        return { data: data.nexResponse }
+      })
+  }
+
+  return {}
+}
 
 export default {
   get(groupBy, from, until, tz) {
@@ -11,14 +25,6 @@ export default {
       until,
       tz
     }
-    return Vue.axiosAuth.get('/api/report/first', { params })
-      .then(firstResponse => {
-        const secondData = { firstPart: firstResponse.data }
-        return Vue.axiosAuth.post('/api/report/second', secondData, { params })
-          .then(secondResponse => {
-            const thirdData = { secondPart: secondResponse.data }
-            return Vue.axiosAuth.post('/api/report/third', thirdData, { params })
-          })
-      })
+    return getInParts(1, params)
   }
 }
